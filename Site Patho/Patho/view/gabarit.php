93074ace230aa83844dc94dae2ec7252dfe7b04b
zutzut333 <61789885+zutzut333@users.noreply.github.com>
2021-05-18 11:09:57 +0200

    header 50%

--- Site Patho/Patho/view/gabarit.php
+++ Site Patho/Patho/view/gabarit.php
@@ -5,11 +5,33 @@
 
 
 <header class = "header">
+
     <div >
         <img src="view/content/image/PathoLogo.png" class="Logo">
     </div>
-    <div class="header">
-      
+
+    <div class="headerTitle">
+        Patho
+    </div>
+
+    <div class="headerTitle">
+        Accueil
+    </div>
+
+    <div class="headerTitle">
+        Films
+    </div>
+
+    <div class="headerTitle">
+        planning
+    </div>
+
+    <div class="headerTitle">
+        contact
+    </div>
+
+    <div class="headerTitle">
+        Login/Register
     </div>
 
 </header>

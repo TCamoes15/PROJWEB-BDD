60816c00fbecace639ae11ec589df0fcca157800
zutzut333 <61789885+zutzut333@users.noreply.github.com>
2021-05-25 09:57:02 +0200

    .

--- SitePatho/Patho/view/Accueil.php
+++ SitePatho/Patho/view/Accueil.php
@@ -9,11 +9,12 @@ ob_start();
 
 <body>
 
-
+<div>
+    <span class="texteSurImage ">Envie de voir le meilleur ? <br> Cliquez juste en bas </span>
     <video autoplay muted loop class="myVideo">
         <source src="view/content/image/TrailerVideo/TrailerStarWars.mp4" type="video/mp4"  >
     </video>
-
+</div>
 
 <div class="filmsPageAccueil">
 

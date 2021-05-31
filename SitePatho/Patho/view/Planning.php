1a5d2066e4094e83ee309b43edc382992dd61018
GUIGNARD Mathias <Mathias.GUIGNARD@cpnv.ch>
2021-05-31 16:04:21 +0200

    planning

--- SitePatho/Patho/view/Planning.php
+++ SitePatho/Patho/view/Planning.php
@@ -60,11 +60,7 @@ ob_clean();
                 </tr>
 
 
-                <!--?php
-                  foreach ($weeks as $week) {
-                    echo $week;
-                  };
-                ?-->
+
             </table>
 
         </div>
@@ -83,74 +79,6 @@ ob_clean();
 
 
     <!-- <?php
-    // Set your timezone!!
-    date_default_timezone_set('Asia/Dhaka');
-
-    // Get prev & next month
-    if (isset($_GET['ym'])) {
-        $ym = $_GET['ym'];
-    } else {
-        // This month
-        $ym = date('Y-m');
-    }
-
-    // Check format
-    $timestamp = strtotime($ym,"-01");
-    if ($timestamp === false) {
-        $timestamp = time();
-    }
-
-    // Today
-    $today = date('Y-m-j', time());
-
-    // For H3 title
-    $html_title = date('M - Y', $timestamp);
-
-    // Create prev & next month link     mktime(hour,minute,second,month,day,year)
-    $prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
-    $next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
-
-    // Number of days in the month
-    $day_count = date('t', $timestamp);
-
-    // 0:Sun 1:Mon 2:Tue ...
-    $str = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
-
-
-    // Create Calendar!!
-    $weeks = array();
-    $week = '';
-
-    // Add empty cell
-    $week .= str_repeat('<td></td>', $str);
-
-    for ( $day = 1; $day <= $day_count; $day++, $str++) {
-
-        $date = $ym.'-'.$day;
-
-        if ($today == $date) {
-            $week .= '<td class="today">'.$day;
-        } else {
-            $week .= '<td>'.$day;
-        }
-        $week .= '</td>';
-
-        // End of the week OR End of the month
-        if ($str % 7 == 6 || $day == $day_count) {
-
-            if($day == $day_count) {
-                // Add empty cell
-                $week .= str_repeat('<td></td>', 6 - ($str % 7));
-            }
-
-            $weeks[] = '<tr>'.$week.'</tr>';
-
-            // Prepare for new week
-            $week = '';
-
-        }
-
-    }
 
     ?>-->
 <?php

ff588d4fa186b5c84ff20ee20ac1a3f586abb9bc
zutzut333 <61789885+zutzut333@users.noreply.github.com>
2021-05-31 10:38:57 +0200

    .

--- SitePatho/Patho/view/LoginRegister.php
+++ SitePatho/Patho/view/LoginRegister.php
@@ -8,28 +8,28 @@ ob_start();
 
 
 
-        <form>
+        <form class="formLogin">
             <h1 class="ConnexionTitleLogin"> Connexion </h1>
-            <h4> E-mail</h4>
-            <input type="search">
-            <h4> Mot-de-passe</h4>
-            <input type="search">
-            <h4> bouton valider </h4>
-            <input type="search">
+            <h4 class="LoginRegisterContentTexte"> E-mail</h4>
+            <input type="search" class="LoginRegisterbutton">
+            <h4 class="LoginRegisterContentTexte"> Mot-de-passe</h4>
+            <input type="search" class="LoginRegisterbutton">
+            <button class="buttonLoginRegisterValidation"> Valider </button>
         </form>
 
-        <form>
+        <form class="formRegister">
             <h1 class="InscriptionTitleLogin"> Inscription </h1>
-            <h4> E-mail</h4>
-            <input type="search">
-            <h4> Mot-de-passe</h4>
-            <input type="search">
-            <h4> Numéro de Téléphone</h4>
-            <input type="search">
-            <h4> Nom</h4>
-            <input type="search">
-            <h4> Prénom</h4>
-            <input type="search">
+            <h4 class="LoginRegisterContentTexte"> E-mail</h4>
+            <input type="search" class="LoginRegisterbutton">
+            <h4 class="LoginRegisterContentTexte"> Mot-de-passe</h4>
+            <input type="search" class="LoginRegisterbutton">
+            <h4 class="LoginRegisterContentTexte"> Numéro de Téléphone</h4>
+            <input type="search" class="LoginRegisterbutton">
+            <h4 class="LoginRegisterContentTexte"> Nom</h4>
+            <input type="search" class="LoginRegisterbutton">
+            <h4 class="LoginRegisterContentTexte">  Prénom</h4>
+            <input type="search" class="LoginRegisterbutton">
+            <button class="buttonLoginRegisterValidation"> Valider </button>
 
         </form>
 
